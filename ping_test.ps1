5a24828c0da176026768baaa93db12da0d633e1c
dannehtran <dannytran141@yahoo.com>
2019-05-02 17:01:11 -0700

    Add files via upload

--- ping_test.ps1
+++ ping_test.ps1
@@ -1,20 +1,32 @@
-﻿$file = Read-Host "Enter the file you wish to use (ie. myCsv.csv, myServer.txt)"
-$extension = [IO.Path]::GetExtension($file)
-if ($extension -eq ".csv") {
-    Import-Csv "$file" -Delimiter "," | Select-Object -ExpandProperty Computer* | Out-File "devices.txt"
+﻿$dir = "pingTestLogs"
+If (-Not (Test-Path -PathType Container "$dir")) {
+    New-Item -ItemType Directory "$dir" | Out-Null
 }
-$servers = "devices.txt"
+
+$file = Read-Host "Enter the file you wish to use (ie. myCsv.csv, myServer.txt)"
+$extension = [IO.Path]::GetExtension($file)
+
+If ($extension -eq ".csv") {
+    Import-Csv "$file" -Delimiter "," | Select-Object -ExpandProperty Computer* | Out-File "$($dir)\devices.txt"
+    $servers = "$($dir)\devices.txt"
+}
+Else {
+    $serverrs = $($file)
+}
+
 $date = Get-Date
-Write-Output $date.ToUniversalTime() | Out-File -Append "online.txt"
-Write-Output $date.ToUniversalTime() | Out-File -Append "offline.txt"
+Write-Output $date.ToUniversalTime() | Out-File -Append "$($dir)\online.txt"
+Write-Output $date.ToUniversalTime() | Out-File -Append "$($dir)\offline.txt"
+
 foreach ($line in Get-Content $servers) {
     $lineFormat = $line.TrimEnd()
-    If (Test-Connection $lineFormat -count 1 -quiet) {
+    If (Test-Connection $lineFormat -Count 1 -Quiet) {
         Write-Output "Host Online: $($line)"
-        Write-Output $line | Out-File -Append "online.txt" 
+        Write-Output $line | Out-File -Append "$($dir)\online.txt" 
     }
     Else {
         Write-Output "Host Offline: $($line)"
-        Write-Output $line | Out-File -Append "offline.txt"
+        Write-Output $line | Out-File -Append "$($dir)\offline.txt"
     }
 }
+Write-Output 
