7388e2e42ddde61b84ea39a05445185f3fce3274
dannehtran <dannytran141@gmail.com>
2021-01-09 22:22:45 -0800

    test code

---
+++ myComputer.py
@@ -0,0 +1,13 @@
+class MyComputer:
+	computer = "Computer Name"
+	gpu = "RTX 3060 TI"
+	cpu = "Intel 9700k"
+
+	def computerSpecs(self, computer, gpu, cpu):
+		self.gpu = gpu
+		self.cpu = cpu
+		return "These are your computer specs: " computer + gpu + cpu
+
+	def __init__(self):
+
+x = MyComputer()
