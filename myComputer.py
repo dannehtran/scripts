0356c3fcb80b85ede54c2f1eae631570c104cfb8
dannehtran <dannytran141@gmail.com>
2021-01-10 02:02:32 -0800

    Added code to class and function

--- myComputer.py
+++ myComputer.py
@@ -1,10 +1,27 @@
+# Computer class that creates the object
 class MyComputer:
-	computer = "Computer Name"
-	gpu = "RTX 3060 TI"
-	cpu = "Intel 9700k"
+	
+	# Initialized functions with madatory parameters of name and user
+	def __init__(self, name, user):
+		self.name = name
+		self.user = user
 
-	def computerSpecs(self, computer, gpu, cpu):
+	# Sets the computer's GPU function with one madatory parameter
+	def setComputerGPU(self, gpu):
 		self.gpu = gpu
-		self.cpu = cpu
-		return "These are your computer specs: " + computer + gpu + cpu
-	x = MyComputer()
+
+# Create an object called X with a name called Danny's Computer and the user as Danny
+x = MyComputer("Danny's Computer", "Danny")
+
+# Calls the function within the class in the object X and sets the GPU as RTX 2070 TI
+x.setComputerGPU("RTX 2070 TI")
+
+# Creates a dictionary that holds the attributes of the computer
+computerDict = {
+	'name' : x.name,
+	'user' : x.user,
+	'gpu' : x.gpu
+}
+
+# Print the attributes of the computer object in a json format
+print(computerDict)
