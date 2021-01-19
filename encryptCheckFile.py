6ccb126eb815c0cc5a65b2706db466d7567dee70
dannehtran <dannytran141@gmail.com>
2021-01-18 22:13:09 -0800

    Removed INIT function and revised some code

--- encryptCheckFile.py
+++ encryptCheckFile.py
@@ -12,8 +12,6 @@ class SecureFile:
 	unencrypted_file = ''
 	encondedKey = ''
 
-	def __init__(self):
-		return
 	# Function that creates a private key in the keys directory.
 	def _createKey(self, keyFile):
 
@@ -45,17 +43,23 @@ class SecureFile:
 				key = newkey.read()
 			return key
 
+	# Function that encrypts a file that takes in two parameters
 	def encryptFile(self, filename, key):
 		self.filename = filename
 		self.key = key
 
+		# Calls the Fernet library to encode the key into bytes
 		encondedKey = Fernet(self.key)
 		
+		# Opens the file we want to encrypt and reads the contents and stores it in a varibale called unencrypted_data
 		with open(filename, 'rb') as file:
 			unencrypted_data = file.read()
+
+		# Encrypts the data and stores it in the encrypted_data variable
 		encrypted_data = encondedKey.encrypt(unencrypted_data)
 		encrypted_file = "encrypted_" + self.filename 
 		
+		# Opens/Creates a new file with the "encrypted" + filename and writes the encrypted_data to the new file
 		with open(encrypted_file, 'wb') as eFile:
 			eFile.write(encrypted_data)
 
@@ -63,30 +67,49 @@ class SecureFile:
 		self.encrypted_data = encrypted_data
 		self.encondedKey = encondedKey
 
+		# Retusn the original file name that it encrypted
 		return filename
 
-	def decryptFile(self):
-		self.unencrypted_file = 'decrypt_' + self.filename
+	# Function that decrypts the encrypted data using an enconded key
+	def decryptFile(self, filename):
+		newFileName = 'decrypt_' + filename
 
-		decrypted_data =  self.encondedKey.decrypt(self.encrypted_data)
-		with open(self.unencrypted_file, "wb") as file:
-			file.write(decrypted_data)
-		return self.unencrypted_file
+		with open(filename, 'rb') as EncryptFile:
+			encrypted_data = EncryptFile.read()
 
+		# Decrpyts the data using the encoded key and stores the result in decrypted_data variable
+		decrypted_data =  self.encondedKey.decrypt(encrypted_data)
 
+		# Opens the unencrypted_file and writes the unencrypted data to it
+		with open(newFileName, "wb") as DecryptFile:
+			DecryptFile.write(decrypted_data)
+
+		# Returns the unencrypted_file name
+		return filename
+
+	# Function that checks the hash of the file
 	def checkHash(self, filename):
 
+		# Open file that you want to check the hash to
 		with open(filename, "rb") as file:
+
+			# Reads the file and stores it in the variable chunk
 			chunk = file.read()
+
+			# Uses SHA256 algorithm to check the hash of the file and store it in hashedData variable
 			hashedData = hashlib.sha256(chunk).hexdigest()
+
+			# Returns the hashed data
 		return(hashedData)
 
-obj = SecureFile()
-keyObj = obj._createKey('test.txt')
-encryptObj = obj.encryptFile('someFile.txt', keyObj)
-hashCheckObj = obj.checkHash(encryptObj)
-decryptObj = obj.decryptFile()
-hashCheckDecryptObj = obj.checkHash(decryptObj)
+obj = SecureFile()												# Creates the class object
+keyObj = obj._createKey('test.txt')								# Calls the _createKey function with some private key
+encryptObj = obj.encryptFile('someFile.txt', keyObj) 			# Calls the encryptFile function with a file and the keyObj
+hashCheckObj = obj.checkHash('encrypted_someFile.txt')						# Checks the hash of the encrypted obj
+print(hashCheckObj)
+decryptObj = obj.decryptFile('encrypted_someFile.txt')									# Decrypts
+hashCheckDecryptObj = obj.checkHash('encrypted_someFile.txt')
+print(hashCheckDecryptObj)
 
 if hashCheckObj == hashCheckDecryptObj:
 	print('Both files have matching hashes, secure file')
